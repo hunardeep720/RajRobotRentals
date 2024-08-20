c34bbb794174eed6a7e35c46ab8b82a935827ac8
Hunardeep Singh <hunardeepsingh@edu.sait.ca>
2024-08-20 15:55:27 -0600

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RajRobotRentals;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RajRobotRentals;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
